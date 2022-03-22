6a4521937a514443c9c990b150263c44c3729075
nnamdi16 <nwabuokeinnamdi@gmail.com>
2022-03-22 23:16:12 +0100

    feat(user-service): Implemented testing on the user controller

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,7 +1,9 @@
 import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { Request } from 'express';
 import { PassportModule } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
+import { tokenPayload, userDetails } from '../../test/services/user-service.spec';
 import { JwtStrategy } from '../common/strategies/jwt.strategy';
 import { PrismaService } from '../prisma.services';
 import { UserController } from './user.controller';
@@ -24,7 +26,18 @@ describe('UserController', () => {
           },
         }),
       ],
-      providers: [UserService, PrismaService, JwtStrategy, ConfigService],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {
+            find: jest.fn().mockResolvedValue([userDetails]),
+            findOne: jest.fn().mockImplementation(() => Promise.resolve(userDetails)),
+          },
+        },
+        PrismaService,
+        JwtStrategy,
+        ConfigService,
+      ],
     }).compile();
     userService = module.get<UserService>(UserService);
     userController = module.get<UserController>(UserController);
@@ -32,6 +45,10 @@ describe('UserController', () => {
 
   it('should be defined', () => {
     expect(userController).toBeDefined();
-    expect(userService).toBeDefined();
+  });
+
+  describe('find Users', () => {
+    it('should fetch all users', async () =>
+      expect(userController.find({}, tokenPayload as Request)).resolves.toEqual([userDetails]));
   });
 });
