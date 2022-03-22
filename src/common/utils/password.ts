628c28db98d47f37573e1d0edde3caeceaffe90b
nnamdi16 <nwabuokeinnamdi@gmail.com>
2022-03-22 23:14:03 +0100

    feat(user-service): Implemented method to hash and compare password

--- src/common/utils/password.ts
+++ src/common/utils/password.ts
@@ -2,8 +2,8 @@ import { NotImplementedException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 
 const saltOrRounds = 10;
-export const hashPassword = async (password: string): Promise<string> => {
-  throw new NotImplementedException();
+export const hashPassword = (password: string) => {
+  return bcrypt.hashSync(password, saltOrRounds);
 };
 
 export const hashPasswordSync = (password: string): string => {
@@ -11,5 +11,5 @@ export const hashPasswordSync = (password: string): string => {
 };
 
 export const matchHashedPassword = async (password: string, hash: string): Promise<boolean> => {
-  throw new NotImplementedException();
+  return bcrypt.compare(password, hash);
 };
