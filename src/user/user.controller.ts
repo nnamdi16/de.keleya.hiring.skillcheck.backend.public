21d082a8e2f451b7a48a377dcea5cfa30a78336f
nnamdi16 <nwabuokeinnamdi@gmail.com>
2022-03-22 23:17:25 +0100

    feat(user-service): Created all endpoints to create user, authenticate, validateToken, getToken, update and delete the user.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,9 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  ParseIntPipe,
-  Patch,
-  Post,
-  Query,
-  Req,
-  HttpCode,
-  UseGuards,
-  NotImplementedException,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, Req, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiBody, ApiHeader, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Request } from 'express';
+import { JwtTokenUser } from '../../src/common/types/jwtTokenUser';
+import { EndpointIsPublic } from '../../src/common/decorators/publicEndpoint.decorator';
+import { JwtAuthGuard } from '../../src/common/guards/jwt-auth.guard';
 import { AuthenticateUserDto } from './dto/authenticate-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { DeleteUserDto } from './dto/delete-user.dto';
@@ -22,46 +12,70 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
+@ApiTags('user')
 export class UserController {
   constructor(private readonly usersService: UserService) {}
 
-  @Get()
+  @Get('/')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   async find(@Query() findUserDto: FindUserDto, @Req() req: Request) {
-    throw new NotImplementedException();
+    return this.usersService.find(req.user as JwtTokenUser, findUserDto);
   }
 
-  @Get(':id')
-  async findUnique(@Param('id', ParseIntPipe) id, @Req() req: Request) {
-    throw new NotImplementedException();
+  @Get('/:id')
+  @ApiBearerAuth()
+  @EndpointIsPublic()
+  @UseGuards(JwtAuthGuard)
+  async findUnique(@Param('id', ParseIntPipe) id: number, @Req() req: Request) {
+    return this.usersService.findUnique({ id }, true, req.user);
   }
 
-  @Post()
+  @Post('/')
+  @ApiResponse({
+    status: 200,
+    description: 'Subrequest created successfully',
+  })
+  @ApiBody({ type: CreateUserDto, description: 'Create a new user with credentials' })
   async create(@Body() createUserDto: CreateUserDto) {
-    throw new NotImplementedException();
+    console.log(createUserDto);
+    return this.usersService.create(createUserDto);
   }
 
   @Patch()
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   async update(@Body() updateUserDto: UpdateUserDto, @Req() req: Request) {
-    throw new NotImplementedException();
+    return this.usersService.update(req.user as JwtTokenUser, updateUserDto);
   }
 
   @Delete()
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   async delete(@Body() deleteUserDto: DeleteUserDto, @Req() req: Request) {
-    throw new NotImplementedException();
+    return this.usersService.delete(req.user as JwtTokenUser, deleteUserDto);
   }
 
   @Post('validate')
+  @EndpointIsPublic()
+  @ApiHeader({
+    name: 'token',
+    description: 'Custom header',
+  })
   async userValidateToken(@Req() req: Request) {
-    throw new NotImplementedException();
+    const { headers } = req;
+    return this.usersService.validateToken(headers.authorization as string);
   }
 
   @Post('authenticate')
+  @EndpointIsPublic()
   async userAuthenticate(@Body() authenticateUserDto: AuthenticateUserDto) {
-    throw new NotImplementedException();
+    return this.usersService.authenticate(authenticateUserDto);
   }
 
   @Post('token')
+  @EndpointIsPublic()
   async userGetToken(@Body() authenticateUserDto: AuthenticateUserDto) {
-    throw new NotImplementedException();
+    return this.usersService.authenticateAndGetJwtToken(authenticateUserDto);
   }
 }
