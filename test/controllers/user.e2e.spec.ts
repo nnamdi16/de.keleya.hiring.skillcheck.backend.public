a89b90ceb9a723fc3506ec4cf19c341fb8478c55
nnamdi16 <nwabuokeinnamdi@gmail.com>
2022-03-23 07:36:32 +0100

    feat(user-service): Removed unused lines of code

--- test/controllers/user.e2e.spec.ts
+++ test/controllers/user.e2e.spec.ts
@@ -1,12 +1,10 @@
 import { PrismaService } from '../../src/prisma.services';
-import { useContainer } from 'class-validator';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../../src/app.module';
 import { CreateUserDto } from '../../src/user/dto/create-user.dto';
 import * as request from 'supertest';
-import { userDetails, userResponse } from '../../test/services/user-service.spec';
-import { User } from '@prisma/client';
+import { userResponse } from '../../test/services/user-service.spec';
 describe('UserController(e2e)', () => {
   let app: INestApplication;
   let prisma: PrismaService;
@@ -20,18 +18,6 @@ describe('UserController(e2e)', () => {
     is_admin: true,
     isDeleted: false,
   };
-  //   const data = expect.objectContaining({
-  //     id: expect.any(Number),
-  //     name: expect.any(String),
-  //     email: expect.any(String),
-  //     email_confirmed: expect.any(Boolean),
-  //     is_admin: expect.any(Boolean),
-  //     created_at: expect.any(Date),
-  //     updated_at: expect.any(Date),
-  //     credentials_id: expect.any(Number),
-  //     isDeleted: expect.any(Boolean),
-  //     credentials: expect.any(String),
-  //   });
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -40,8 +26,6 @@ describe('UserController(e2e)', () => {
     app = moduleFixture.createNestApplication();
     prisma = app.get<PrismaService>(PrismaService);
 
-    // useContainer(app.select(AppModule), { fallbackOnErrors: true });
-    // app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
 
     await prisma.user.create({
